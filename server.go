f5d11b1193a780cdcdc653772e1433c53dbd2607
Nemuel Wainaina <bR0S3@protonmail.com>
2023-09-17 06:59:43 +0300

    Define controller functions

--- server.go
+++ server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/teris-io/shortid"
 )
@@ -17,9 +20,23 @@ func main() {
 }
 
 func shortenURL(c *gin.Context) {
+	longURL := c.PostForm("longURL")
+	id, _ := shortid.Generate()
+	host := c.Request.Host
 
+	shortURL := fmt.Sprintf("https://%s/%s", host, id)
+	urlMap[id] = longURL
+
+	c.JSON(http.StatusOK, gin.H{"shortURL": shortURL})
 }
 
 func redirectToURL(c *gin.Context) {
+	hash := c.Param("hash")
+	longURL, ok := urlMap[hash]
+	if !ok {
+		c.JSON(http.StatusNotFound, gin.H{"error": "URL not found"})
+		return
+	}
 
+	c.Redirect(http.StatusPermanentRedirect, longURL)
 }
